512f23f94c07b8ea9911cae04cd141db9d450501
Taft-Brigham <taftbrigham@gmail.com>
2024-10-13 01:48:39 +0000

    made lost of changes

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,24 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// function addProductToCart(product) {
+//   setLocalStorage("so-cart", product);
+// }
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
+
+const productId = getParam("product");
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
